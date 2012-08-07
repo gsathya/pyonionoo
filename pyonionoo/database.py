218e2d7d4a1c430a96c65b76811626cc8a54e3d4
Megan <mchang01@wesleyan.edu>
2012-08-07 14:41:52 -0400

    Added sqlite database functionality:
    Created a database to store the information of each router according to
    the different fields. One table (summary) contains all the fields except
    for 'flags' and 'addresses'. A database and the summary table is created
    using database(). get_summary_routers() takes in the arguments from
    SummaryHandler, reads the summary table, and appends the necessary
    routers to a tuple of lists that will be passed back to the SummaryHandler.
    'flags' and 'addresses' will be in their own separate tables. The next step
    will be to add these two tables to the database.

--- pyonionoo/database.py
+++ pyonionoo/database.py
@@ -1,21 +1,63 @@
 import datetime
+import sqlite3
+
+import logging
+logger = logging.getLogger(__name__)
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
 
 from pyonionoo.parser import Router
 
 ARGUMENTS = ['type', 'running', 'search', 'lookup', 'country', 'order', 'offset', 'limit']
 
-SUMMARY = '/home/mchang01/HFOSS2012/out/summary'
+SUMMARY = '/home/mchang01/HFOSS2012/pyonionoo/pyonionoo/summary'
 
-def get_summary_router(arguments):
+CURSOR = None
+
+def database():
+    logger.info('Creating database.')
+    global CURSOR
     
+    conn = sqlite3.connect('summary.db')
+    CURSOR = conn.cursor()
+    CURSOR.execute('DROP TABLE IF EXISTS summary')
+    CURSOR.execute('CREATE TABLE summary(id INTEGER PRIMARY KEY AUTOINCREMENT, type CHARACTER, nickname STRING, fingerprint STRING, running BOOLEAN, time_published STRING, OR_port STRING, dir_port STRING, consensus_weight STRING, country_code STRING, hostname STRING, time_lookup STRING)')
+    
+    columns = ('type', 'nickname', 'fingerprint', 'running', 'time_published', 'OR_port', 'dir_port', 'consensus_weight', 'country_code', 'hostname', 'time_lookup')
+    insert_stmt = 'INSERT INTO summary ({}) VALUES ({})'.format(','.join(columns),
+            ','.join(['?']*len(columns)))
+    with open(SUMMARY) as f:
+        router_tuples = []
+        for line in f.readlines():
+            router = Router(line)
+            logging.debug('Adding {} to database.'.format(router.nickname))
+            
+            if router.is_relay:
+                router_type = 'r'
+            elif not router.is_relay:
+                router_type = 'b'
+            
+            router_tuples.append((router_type, router.nickname, router.fingerprint, router.is_running, router.time_published, router.orport, router.dirport, router.consensus_weight, router.country_code, router.hostname, router.time_of_lookup))
+        CURSOR.executemany(insert_stmt, router_tuples)
+        conn.commit()
+        # CURSOR.close()
+
+    logging.info('Finished creating database.')
+
+def get_summary_routers(arguments):
+    
+    summary_database = database()
+
     routers = []
-    filtered_relays, filtered_bridges = [], []
     return_relays, return_bridges = True, True
     hex_fingerprint, running_filter = None, None
     return_type, return_country, return_search = False, False, False
     relay_timestamp = datetime.datetime(1900, 1, 1, 1, 0)
     bridge_timestamp = datetime.datetime(1900, 1, 1, 1, 0)
-    #TODO: Externalize filtering code?
+
     for key, values in arguments.iteritems():
         if key in ARGUMENTS:
             if key == "running":
@@ -38,47 +80,50 @@ def get_summary_router(arguments):
             if key == "search":
                 for value in values:
                     return_search = True
-                    search_input = value
-    
-    with open(SUMMARY) as f:
-        
-        for line in f.readlines():
-            router = Router(line)
-            
-            if router.is_relay and return_relays:
-                dest = filtered_relays
-                time_dest = relay_timestamp
-            elif not router.is_relay and return_bridges:
-                dest = filtered_bridges
-                time_dest = bridge_timestamp
-            else:
-                continue
-            if return_type:
-                dest.append(router)
-            elif running_filter:
-                if router.is_running == eval(running_filter.title()):
-                    dest.append(router)
-            elif hex_fingerprint:
-                if hex_fingerprint == router.fingerprint:
-                    dest.append(router)
-                    break
-            elif return_country:
-                if router.country_code == country_code:
-                    dest.append(router)
-            elif return_search:
-                if search_input in router.nickname:
-                    dest.append(router)
-                if search_input in router.fingerprint:
-                    dest.append(router)
-                if search_input in router.address:
-                    dest.append(router)
-            else:
-                dest.append(router)
-            
-            current_router = router.time_published
-            if current_router > time_dest:
-                time_dest = current_router
+                    search_input = value            
 
-        total_routers = (filtered_relays, filtered_bridges,
-                         relay_timestamp, bridge_timestamp)
-        return total_routers
+    filtered_relays, filtered_bridges = [], []
+    for row in CURSOR.execute('SELECT type, nickname, fingerprint, running, country_code, time_published FROM summary'):
+        if row[0] == 'r' and return_relays:
+            dest = filtered_relays
+        elif row[0] == 'b' and return_bridges:
+            dest = filtered_bridges
+        else:
+            continue
+        if return_type:
+            dest.append(row)
+        elif running_filter:
+            if running_filter.title() == 'True':
+                running = True
+            elif running_filter.title() == 'False':
+                running = False
+            else: raise ValueError('A proper boolean value was not provided.')
+            if row[3] == running:
+                dest.append(row)
+        elif hex_fingerprint:
+            if hex_fingerprint == row[2]:
+                dest.append(row)
+                break
+        elif return_country:
+            if country_code == row[4]:
+                dest.append(row)
+        elif return_search:
+            if search_input in row[1]:
+                dest.append(row)
+            if search_input in row[2]:
+                dest.append(row)
+            # ADDRESS SEARCH
+        else:
+            dest.append(row)
+
+        current_router = datetime.datetime.strptime(row[5], "%Y-%m-%d %H:%M:%S")
+        if row[0] == 'r':
+            if current_router > relay_timestamp:
+                relay_timestamp = current_router
+        if row[0] == 'b':
+            if current_router > bridge_timestamp:
+                bridge_timestamp = current_router
+
+    total_routers = (filtered_relays, filtered_bridges, relay_timestamp, bridge_timestamp)
+    return total_routers
+
--- pyonionoo/handlers/summary.py
+++ pyonionoo/handlers/summary.py
@@ -16,16 +16,25 @@ class SummaryHandler(cyclone.web.RequestHandler):
     def get(self, foo):
 
         user_arguments = self.request.arguments
-        routers = database.get_summary_router(user_arguments)
+        routers = database.get_summary_routers(user_arguments)
 
         response = {}
         relays, bridges = [], []
         filtered_relays, filtered_bridges, relay_timestamp, bridge_timestamp = routers
-        
-        for (src, dest) in [(filtered_relays, relays),(filtered_bridges, bridges)]:
-            for router in src:
-                dest.append({'n': router.nickname, 'f': router.fingerprint, 'a': [router.address], 'r': router.is_running})
 
+        for (router_type, nickname, fingerprint, running, country_code, time_published) in filtered_relays:
+            if running == 0:
+                running_str = 'false'
+            if running == 1:
+                running_str = 'true' 
+            relays.append({'n': nickname, 'f': fingerprint, 'r': running_str})
+
+        for (router_type, nickname, fingerprint, running, country_code, time_published) in filtered_bridges:   
+            if running == 0:
+                running_str = 'false'
+            if running == 1:
+                running_str = 'true' 
+            bridges.append({'n': nickname, 'f': fingerprint, 'r': running_str})
 
         if relays:
             response['relays'] = relays
@@ -35,3 +44,4 @@ class SummaryHandler(cyclone.web.RequestHandler):
             response['bridges_published'] = bridge_timestamp.strftime("%Y-%m-%d %H:%M:%S")
 
         self.write(response)
+
